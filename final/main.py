3f2757abec2251cdf0ee057a1229e4e99c8f59ff
CSSI <css@CSSs-MacBook-Air.local>
2017-08-01 14:24:48 -0500

    update

--- final/main.py
+++ final/main.py
@@ -14,6 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+from google.appengine.api import users
 import webapp2
 import jinja2
 import os
@@ -51,6 +52,17 @@ class SearchHandler(webapp2.RequestHandler):
         self.response.write("search works")
         ingredient = self.request.get("name")
         self.response.write(ingredient)
+class LoginHandler(webapp2.RequestHandler):
+    def get(self):
+        user = users.get_current_user()
+        if user:
+            greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+                (user.nickname(), users.create_logout_url('/')))
+        else:
+            greeting = ('<a href="%s">Sign in or register</a>.' %
+                users.create_login_url('/'))
+
+        self.response.write('<html><body>%s</body></html>' % greeting)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
