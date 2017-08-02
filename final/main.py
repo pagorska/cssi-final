950c0c8241a409b971d6c64aa0301d8b630923d0
pagorska <patrycja.gorska03@gmail.com>
2017-08-02 10:36:03 -0500

    Deleted fridge, made it the home page

--- final/main.py
+++ final/main.py
@@ -97,16 +97,11 @@ class LoginHandler(webapp2.RequestHandler):
                 users.create_login_url('/'))
         self.response.write('<html><body>%s</body></html>' % greeting)
 
-class FridgeHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('fridge works!')
-
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/search', SearchHandler),
     ('/about-us', AboutHandler),
     ('/restaurants', RestaurantHandler),
-    ('/login', LoginHandler),
-    ('/fridge', FridgeHandler)
+    ('/login', LoginHandler)
 
 ], debug=True)
