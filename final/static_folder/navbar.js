79d3ff479b764f7e41f0c4d83d8d941b78e867f1
CSSI <css@CSSs-MacBook-Air.local>
2017-08-02 09:43:54 -0500

    update

--- final/static_folder/navbar.js
+++ final/static_folder/navbar.js
@@ -1,14 +1,7 @@
 function disappear() {
   $("#input").toggleClass("hidden");
 }
-
 $(document).ready(function() {
-  $("#popdown").click();
-  $("#about").click();
-  $("#home").click();
-  $("#recipes").click();
-  $("#map").click();
-  $("#fridge").click();
   $("#add").click(disappear);
   }
 )
